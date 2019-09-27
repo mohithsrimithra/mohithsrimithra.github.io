b09bffff94648efcc7d60307a755b0d3a9c94f15
mohithsrimithra <55794446+mohithsrimithra@users.noreply.github.com>
2019-09-27 15:19:10 +0530

    Create datatypes.java

---
+++ java/datatypes.java
@@ -0,0 +1,17 @@
+public class DataTypes
+{
+ public static void main(String[] arrgs)
+ {
+  int x=10;
+  System.out.println(x);
+  char d;
+  d='c';
+  System.out.println(d);
+  String name="chodagudi";
+  System.out.println(name);
+  boolean mybool=12>13;
+  System.out.println(mybool);
+  float s=0.44f;
+  System.out.println(s);
+ }
+}
