c4c60a84ab6cfb0ccfd20a2270838de80d76383e
mohithsrimithra <55794446+mohithsrimithra@users.noreply.github.com>
2019-09-28 15:18:33 +0530

    Create strings.java

---
+++ java/strings.java
@@ -0,0 +1,14 @@
+public class Strings
+{
+  public static void main(String[] arrgs)
+  {
+    String name="chodagudi";
+    String name1="mitra";
+    System.out.println("name" = name);
+    System.out.println(name.length());
+    System.out.println(name.toUpperCase());
+    System.out.println(name1.toUpperCase());
+    System.out.println(name+" "+name1);
+    System.out.println(name.Concat(name1));
+  }
+}
